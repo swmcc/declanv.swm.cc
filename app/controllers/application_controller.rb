5fb9174875b9b2e32bb1157a62200431476a9dd5
Stephen McCullough <steve@shopkeep.com>
2016-11-24 20:32:00 +0000

    Added simple about page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def about
+    render template: 'application/about'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations"}
   root to: 'games#index'
   resources :games
+  get "/about" => "application#about"
 end
