d77e14142221841f64f5d64680a7a103ceeb80bb
Stephen McCullough <steve@shopkeep.com>
2016-11-24 19:17:24 +0000

    Created swm_wins and declan_wins method. Amended index page to include top tally. Added some documentation at the bottom of the page

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,13 @@
 class Game < ApplicationRecord
+
+  SWM_PRE_EPOCH_WINS = 29.freeze
+  DECLAN_PRE_EPOCH_WINS = 21.freeze
+
+  def self.swm_wins
+     where(winner: 'SWM').count + SWM_PRE_EPOCH_WINS
+  end
+
+  def self.declan_wins
+     where(winner: 'Declan').count + DECLAN_PRE_EPOCH_WINS
+  end
 end
