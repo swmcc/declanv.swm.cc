ccf02699e5f0fe76aa2de4aa9e1cae603481c2e6
Stephen McCullough <steve@shopkeep.com>
2016-11-24 20:05:43 +0000

    Don't show the links for CRUD unless logged in.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update]
+  before_action :authenticate_user!, :except => [:index, :show]
 
   def index
     @games = Game.all
